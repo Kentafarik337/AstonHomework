7078718109978acb8414904422171a0cc688b881
Kentafarik337 <levoshinegor@yandex.ru>
2025-10-20 16:30:12 +0400

    добавил Itemlist как дженерик для всего, и в postcard сделал через дженерик. Так же добавил в app для vite конфига

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import './PostCard.css';
 import { CommentList } from '../../../widgets/CommentList/ui/CommetnList';
 import { UserTabs } from '../../../widgets/UserTabs/UserTabs';
-export interface Post {
+
+export interface BasePost {
   id: number;
   title: string;
   body: string;
   iduser: number;
 }
 
-interface PostCardProps {
-  post: Post;
+interface PostCardProps<T extends BasePost> {
+  post: T;
+  renderExtra?: (post: T) => React.ReactNode;
 }
 
-export const PostCard: React.FC<PostCardProps> = ({ post }) => {
+export const PostCard = <T extends BasePost>({ post, renderExtra }: PostCardProps<T>): React.ReactElement => {
   return (
     <div className="post-card">
       <h3>{post.title}</h3>
@@ -23,6 +25,7 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
       <CommentList postId={post.id} />
       <div className="post-meta">
         <span>ID: {post.id}</span>
+        {renderExtra && renderExtra(post)}
       </div>
     </div>
   );
